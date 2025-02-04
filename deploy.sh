2a1564349b14ad8a1eaa2e62aac075742fc36b03
U.Thejus <thejusu.22msc@kongu.edu>
2025-02-04 18:57:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u thejus7 -p Thaladhoni@7
-    docker tag test thejus7/ar
-    docker push thejus7/ar
+    docker tag test thejus7/test
+    docker push thejus7/test
     
