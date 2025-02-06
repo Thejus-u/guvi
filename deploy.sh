5acfc2fa5297a99f20a12bd0c93303df657a909a
U.Thejus <thejusu.22msc@kongu.edu>
2025-02-06 14:02:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u thejus7 -p Thaladhoni@7
+    docker login -u thejus7 -p dckr_pat_je4Q6CnC2mD7ln_2S7HAtngJ6ng
     docker tag test thejus7/test
     docker push thejus7/test
     kubectl create deployment jen --image=thejus7/test --port=80
