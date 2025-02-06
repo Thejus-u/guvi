aa90f3a6d52485a97886a9a4065568574f70b531
U.Thejus <thejusu.22msc@kongu.edu>
2025-02-06 13:49:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@
     docker login -u thejus7 -p Thaladhoni@7
     docker tag test thejus7/test
     docker push thejus7/test
-    
+    kubectl create deployment jen --image=thejus7/test --port=80
+     kubectl expose deployment jen --type=NodePort --port=80
