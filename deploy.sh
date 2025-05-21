028b887aaf2ddacafa3e7fdbd3950b5b2688b61a
U.Thejus <thejusu.22msc@kongu.edu>
2025-05-21 14:44:34 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u thejus7 -p dckr_pat_je4Q6CnC2mD7ln_2S7HAtngJ6ng
-    docker tag test thejus7/test
-    docker push thejus7/test
-   
